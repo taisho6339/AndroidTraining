66180e4689555a51a16ce8f78a446fc51e178c4c
TaishoSakamoto <taisho6339@gmail.com>
2013-06-16 22:09:30 +0900

    Update MainActivity.java

--- assignments/fundamentals/2nd/ControllerAssignment3/src/jp/mixi/assignment/controller/adv/MainActivity.java
+++ assignments/fundamentals/2nd/ControllerAssignment3/src/jp/mixi/assignment/controller/adv/MainActivity.java
@@ -1,14 +1,28 @@
-
 package jp.mixi.assignment.controller.adv;
+
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 
 public class MainActivity extends FragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }  
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		FragmentManager manager = getSupportFragmentManager();
+		// FragmentTransaction を開始
+		FragmentTransaction transaction = manager.beginTransaction();
+
+		// FragmentContainer のレイアウトに、MyFragment を割当てる
+		transaction.add(R.id.FragmentContainer, MyFragment1.createInstance());
+
+
+		// 変更を確定して FragmentTransaction を終える
+		transaction.commit();
+	}
+
 }
