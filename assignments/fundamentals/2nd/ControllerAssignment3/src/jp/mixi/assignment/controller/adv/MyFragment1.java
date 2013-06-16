22e8cdf7cd86bcc50c6740f84d691beaeb6090be
TaishoSakamoto <taisho6339@gmail.com>
2013-06-16 22:08:54 +0900

    MyFragment

--- assignments/fundamentals/2nd/ControllerAssignment3/src/jp/mixi/assignment/controller/adv/MyFragment1.java
+++ assignments/fundamentals/2nd/ControllerAssignment3/src/jp/mixi/assignment/controller/adv/MyFragment1.java
@@ -15,7 +15,16 @@ public class MyFragment1 extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// LayoutInflater を利用して、レイアウトをリソースとして読み込む
-		View view = inflater.inflate(R.layout.fragment1, container, false);
+		View view = inflater
+				.inflate(R.layout.fragment_layout, container, false);
 		return view;
+	}
+
+	// 初期化専用のメソッド。Fragment に対する初期化用の引数はここで管理する
+	public static Fragment createInstance() {
+		Fragment fragment = new MyFragment1();
+		
+		// 新しいインスタンスを返す
+		return fragment;
 	}
 }
