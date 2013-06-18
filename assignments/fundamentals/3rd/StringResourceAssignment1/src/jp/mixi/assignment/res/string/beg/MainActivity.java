150b149a4b49ae773c179ebd1c3509beb3f58bc1
taisho6339 <taisho6339@taisho6339-UX31A>
2013-06-18 10:58:10 +0900

    #課題1

--- assignments/fundamentals/3rd/StringResourceAssignment1/src/jp/mixi/assignment/res/string/beg/MainActivity.java
+++ assignments/fundamentals/3rd/StringResourceAssignment1/src/jp/mixi/assignment/res/string/beg/MainActivity.java
@@ -1,16 +1,21 @@
-
 package jp.mixi.assignment.res.string.beg;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-        // TODO 1 個のものと、複数個のものの両方を並べて表示するため、両方用の TextView を取り出し、リソースへアクセスして表示する
-    }
+		// TODO 1 個のものと、複数個のものの両方を並べて表示するため、両方用の TextView を取り出し、リソースへアクセスして表示する
+		TextView text1 = (TextView) findViewById(R.id.PluralsTextForOne);
+		TextView text2 = (TextView) findViewById(R.id.PluralsTextForOther);
+
+		text1.setText(getResources().getQuantityText(R.plurals.hello_world, 1));
+		text2.setText(getResources().getQuantityText(R.plurals.hello_world, 2));
+	}
 }
