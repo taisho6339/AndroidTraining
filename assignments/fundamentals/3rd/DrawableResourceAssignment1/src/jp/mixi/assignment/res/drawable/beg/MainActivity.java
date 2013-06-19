0a7e54e0be68762a85b05ed6f129dd03de0ef459
taisho6339 <taisho6339@taisho6339-UX31A>
2013-06-19 09:54:03 +0900

    3章課題

--- assignments/fundamentals/3rd/DrawableResourceAssignment1/src/jp/mixi/assignment/res/drawable/beg/MainActivity.java
+++ assignments/fundamentals/3rd/DrawableResourceAssignment1/src/jp/mixi/assignment/res/drawable/beg/MainActivity.java
@@ -1,23 +1,37 @@
-
 package jp.mixi.assignment.res.drawable.beg;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	@Override
+	public void onStart() {
+		super.onStart();
+		ImageView image = (ImageView) findViewById(R.id.image);
+		image.startAnimation(AnimationUtils.loadAnimation(this,
+				R.anim.animation));
+	}
+
+	public void onClickButton(View v) {
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
 }
