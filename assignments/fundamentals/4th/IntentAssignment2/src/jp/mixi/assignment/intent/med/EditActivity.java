13f36c6df87957310fcd821c5923d8303fe0fcfa
taisho6339 <taisho6339@taisho6339-UX31A>
2013-06-19 10:39:16 +0900

    課題 intent結果を受け取る

--- assignments/fundamentals/4th/IntentAssignment2/src/jp/mixi/assignment/intent/med/EditActivity.java
+++ assignments/fundamentals/4th/IntentAssignment2/src/jp/mixi/assignment/intent/med/EditActivity.java
@@ -1,21 +1,31 @@
 package jp.mixi.assignment.intent.med;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.EditText;
 
 public class EditActivity extends Activity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit);
 
-        findViewById(R.id.Submit).setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // TODO ここで、id が MyEdit の EditText からテキストを取得して、結果にセットする
-            }
-        });
-    }
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_edit);
+
+		findViewById(R.id.Submit).setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+
+				// TODO ここで、id が MyEdit の EditText からテキストを取得して、結果にセットする
+				EditText text = (EditText) findViewById(R.id.MyEdit);
+				Intent intent = new Intent();
+				intent.putExtra("MESSAGE", text.getText().toString());
+				setResult(RESULT_OK, intent);
+				finish();
+			}
+		});
+	}
 }
--- assignments/fundamentals/4th/IntentAssignment2/src/jp/mixi/assignment/intent/med/MainActivity.java
+++ assignments/fundamentals/4th/IntentAssignment2/src/jp/mixi/assignment/intent/med/MainActivity.java
@@ -1,24 +1,49 @@
-
 package jp.mixi.assignment.intent.med;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	final int REQUEST_CODE = 1;
 
-        View button = findViewById(R.id.CallActivityForResult);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // TODO EditText と ボタンを 1 つずつ持つ新しい Activity を呼び出し、ボタンを押した時に結果を返すように実装する
-                // TODO 返ってきた結果を Toast で表示するところも実装すること
-            }
-        });
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		View button = findViewById(R.id.CallActivityForResult);
+		button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO EditText と ボタンを 1 つずつ持つ新しい Activity
+				// を呼び出し、ボタンを押した時に結果を返すように実装する
+				// TODO 返ってきた結果を Toast で表示するところも実装すること
+
+				Intent intent = new Intent(MainActivity.this,
+						EditActivity.class);
+				startActivityForResult(intent, REQUEST_CODE);
+			}
+		});
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+		super.onActivityResult(requestCode, resultCode, intent);
+		String answer = intent.getStringExtra("MESSAGE");
+		if (resultCode == RESULT_OK) {
+			switch (requestCode) {
+			case REQUEST_CODE:
+				Toast.makeText(MainActivity.this, answer, Toast.LENGTH_SHORT).show();
+				break;
+			default:
+				Toast.makeText(this, "ERROR", Toast.LENGTH_SHORT).show();
+				break;
+			}
+		}
+
+	}
 }
