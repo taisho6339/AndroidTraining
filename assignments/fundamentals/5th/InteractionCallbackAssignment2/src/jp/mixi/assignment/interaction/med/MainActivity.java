77ee58c86c33ede519472d0d454223ee12401773
taisho6339 <taisho6339@taisho6339-UX31A>
2013-06-20 02:06:30 +0900

    文字数カウント

--- assignments/fundamentals/5th/InteractionCallbackAssignment2/src/jp/mixi/assignment/interaction/med/MainActivity.java
+++ assignments/fundamentals/5th/InteractionCallbackAssignment2/src/jp/mixi/assignment/interaction/med/MainActivity.java
@@ -1,23 +1,56 @@
-
 package jp.mixi.assignment.interaction.med;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	EditText edit;
+	private TextWatcher mTextWatcher = new TextWatcher() {
+		@Override
+		public void onTextChanged(CharSequence s, int start, int before,
+				int count) {
+		}
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+		@Override
+		public void beforeTextChanged(CharSequence s, int start, int count,
+				int after) {
+		}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+		@Override
+		public void afterTextChanged(Editable s) {
+			int count =edit.length();
+			TextView text = (TextView)findViewById(R.id.text);
+			text.setText(count+"文字");
+		}
+	};
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+	}
+
+	public void onStart() {
+		super.onStart();
+		edit =(EditText)findViewById(R.id.edit);
+		edit.addTextChangedListener(mTextWatcher);
+	}
+
+	public void onStop(){
+		super.onStop();
+		edit.removeTextChangedListener(mTextWatcher);
+	}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
 }
